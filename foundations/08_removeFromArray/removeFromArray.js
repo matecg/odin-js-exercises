b91123c520f103e9392fc114635a7bef839379f7
Gody <250132528+matecg@users.noreply.github.com>
2026-02-23 15:19:02 -0300

    Add foundations exercise 08 solution

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
 const removeFromArray = function() {
+    const toExclude = [...arguments].slice(1);
+    let output = [];
+  
+    for (const el of arguments[0]) {
+        if (toExclude.includes(el)) continue;
+        output.push(el);
+    }
+
+    return output;
 };
 
 // Do not edit below this line
