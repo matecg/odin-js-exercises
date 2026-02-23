d6a07be0fea7850c0a42b7959ed9bafd17c3e91b
Gody <250132528+matecg@users.noreply.github.com>
2026-02-23 15:31:57 -0300

    Add foundations exercise 09 solution

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (isNaN(a) || isNaN(b)) return 'ERROR';
+    if (typeof(a) !== "number" || typeof(b) !== "number") return 'ERROR';
+    if (!Number.isInteger(a) || !Number.isInteger(b)) return 'ERROR';
+    if (a < 0 || b < 0) return 'ERROR';
 
+    return (a + b) * (Math.abs(b - a) + 1) / 2;
 };
 
 // Do not edit below this line
