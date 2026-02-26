b50f8e3e426a05280b6daf30f22a87e6bc3707bb
Gody <250132528+matecg@users.noreply.github.com>
2026-02-26 06:28:08 -0300

    Add '12-calculator' exercise solution

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,33 +1,35 @@
-const add = function() {
-	
+const add = function (a, b) {
+    return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+    return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (numbers) {
+    return numbers.reduce((acc, cur) => acc + cur, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (numbers) {
+    return numbers.reduce((acc, cur) => acc * cur);
 };
 
-const power = function() {
-	
+const power = function (base, exponent) {
+    return base ** exponent
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+    let output = 1;
+    for (let i = 1; i <= num; output *= i, i++);
+    return output;
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial
 };
