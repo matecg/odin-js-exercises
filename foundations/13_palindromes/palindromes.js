ffc8e201f9e07d0bda9a751fbc5589f4dc91af49
Gody <250132528+matecg@users.noreply.github.com>
2026-02-26 06:55:30 -0300

    Add '13-palindromes' exercise solution

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    // First we must remove non alphanumerical characters from the string
+    const charOnly = string.replace(/[^a-zA-Z0-9]/g, '').toLowerCase().split('');
+    const reversed = charOnly.slice().reverse().join('');
+    return charOnly.join('') === reversed;
 };
 
 // Do not edit below this line
