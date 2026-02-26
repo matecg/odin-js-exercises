659c5983b3308dcc45a552be4d0da512c4cc627d
Gody <250132528+matecg@users.noreply.github.com>
2026-02-26 07:30:17 -0300

    Add exercise '16-find the oldest' solution

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const sorted = people
+        .sort((pepA, pepB) => {
+            const currentYear = new Date().getFullYear();
+            const aAge = (pepA.yearOfDeath ? pepA.yearOfDeath : currentYear) - pepA.yearOfBirth;
+            const bAge = (pepB.yearOfDeath ? pepB.yearOfDeath : currentYear) - pepB.yearOfBirth;
+            return bAge - aAge;
+        });
+    
+    return sorted[0];
 };
 
 // Do not edit below this line
