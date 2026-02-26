e3ca15657ef926afec45130e254c532a5e172475
Gody <250132528+matecg@users.noreply.github.com>
2026-02-26 07:15:57 -0300

    Add exercise '14-fibonacci' solution

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = Number(n);
+    if (n < 0) return "OOPS"
+    if (n === 1 || n === 2) return 1;
+    if (n === 0) return 0;
+    let current = 1;
+    for (let i = 2, previous = 1; i < n; i++) {
+        current += previous;
+        previous = current - previous;
+    }
+    return current;
+    // Recursion is slower than loops for higher inputs
+    //return fibonacci(n - 1) + fibonacci(n - 2);
 };
 
 // Do not edit below this line
